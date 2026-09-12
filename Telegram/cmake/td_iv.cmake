a480a58640da5c9f9a647bf733bbcc05224d0f9b
gabov <gabov@users.noreply.github.com>
2026-09-12 17:41:12 +0500

    fix(cmake): suppress MicroTeX C5038, C4265, C4005 warnings on MSVC with Ninja

--- Telegram/cmake/td_iv.cmake
+++ Telegram/cmake/td_iv.cmake
@@ -13,11 +13,15 @@ target_compile_options(td_iv_reorder_warning_off
 INTERFACE
     $<$<CXX_COMPILER_ID:Clang,AppleClang>:-Wno-reorder-ctor>
     $<$<CXX_COMPILER_ID:GNU>:-Wno-reorder>
+    $<$<CXX_COMPILER_ID:MSVC>:/wd5038 /wd4265 /wd4005>
 )
 
-if (MSVC AND CMAKE_GENERATOR MATCHES "^Visual Studio ")
-    set_property(TARGET td_iv APPEND PROPERTY VS_PROJECT_IMPORT
-        ${CMAKE_CURRENT_LIST_DIR}/td_iv_msvc_warning_suppressions.props)
+if (MSVC)
+    target_compile_options(td_iv PRIVATE /wd5038 /wd4265 /wd4005)
+    if (CMAKE_GENERATOR MATCHES "^Visual Studio ")
+        set_property(TARGET td_iv APPEND PROPERTY VS_PROJECT_IMPORT
+            ${CMAKE_CURRENT_LIST_DIR}/td_iv_msvc_warning_suppressions.props)
+    endif()
 endif()
 
 target_precompile_headers(td_iv PRIVATE ${src_loc}/iv/iv_pch.h)
